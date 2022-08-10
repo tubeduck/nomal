0fc6a339aecd50f51ff05b11f2f09ede21189681
tubeduck <ilseo1023@gmail.com>
2022-08-10 12:08:13 +0900

    2

---
+++ main.js
@@ -0,0 +1,8 @@
+const buggerBtn = document.querySelector('.navbar_hamburger');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_icons');
+
+buggerBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
